2c1b6a8ebe68cdc55213d743bf98ad7c514bf972
BillMills <mills.wj@gmail.com>
2019-02-17 00:08:43 -0500

    add springfox / swagger docs

--- api/src/main/java/tableofnuclides/tonController.java
+++ api/src/main/java/tableofnuclides/tonController.java
@@ -2,18 +2,25 @@ package tableofnuclides;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
+@RequestMapping("/demo")
+@Api(value="demoapi", description="A demo API block")
 public class tonController {
 
-    @RequestMapping("/elt")
-    public Element z(@RequestParam(value="Z", defaultValue="1") int Z) {
+    @ApiOperation(value = "Fetch the element corresponding to an atomic number", response = Iterable.class)
+    @RequestMapping(value = "/elt", method = RequestMethod.GET)
+    public Element elt(@RequestParam(value="Z", defaultValue="1") int Z) {
         return new Element(Z);
     }
 
-    @RequestMapping("/price")
-    public Dblookup z(@RequestParam(value="name", defaultValue="widget") String n) {
+    @ApiOperation(value = "Fetch the price of a product by name", response = Iterable.class)
+    @RequestMapping(value = "/price", method = RequestMethod.GET)
+    public Dblookup price(@RequestParam(value="name", defaultValue="widget") String n) {
         return new Dblookup(n);
     }
 }
